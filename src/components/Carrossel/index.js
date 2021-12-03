56676f2f29ccb91831019b5973774fbb35e71be4
EstherPimentel <85425371+EstherPimentel@users.noreply.github.com>
2021-12-03 13:42:56 -0300

    atualização 6?

--- src/components/Carrossel/index.js
+++ src/components/Carrossel/index.js
@@ -1,73 +1,71 @@
-import {Carousel } from 'react-bootstrap';
+import {Carousel, Row, Col } from 'react-bootstrap';
 import cacto02 from '../../imgs/produtos/cactos/cacto02.jpg';
 import suculenta2 from '../../imgs/produtos/suculentas/suculenta2.jpg';
 import samambaia from '../../imgs/home/categoriaSamambaia.jpg';
 import flor04 from '../../imgs/produtos/flores/flor04.png';
 import designSemNome2 from '../../imgs/produtos/jardinagem/designSemNome2.png';
-import {useState} from 'react';
 import './style.scss';
 
-const listaProjetos = [
-    {
-      id: 1,
-      imagem:cacto02,
-      nome: "cacto"
-    },
-    {
-      id: 2,
-      imagem:suculenta2,
-      nome: "suculenta"
-    },
-    {
-      id: 3,
-      imagem:samambaia,
-      nome: "samambaia"
-    },
-    {
-      id: 4,
-      imagem:flor04,
-      nome: "flor"
-    },
-    {
-      id: 5,
-      imagem:designSemNome2,
-      nome: "utensílios"
-    },
-    {
-      id: 6,
-      imagem:cacto02,
-      nome: "cacto"
-    }
-]    
+const Carrossel = () =>{
 
-const Carrossel= () =>{
-    const [index, setIndex] = useState(0);
-    const handleSelect = (selectedIndex, e) => {
-      setIndex(selectedIndex);
-    };
-
-    return (
-      <section id="carrossel">
-        <h2>Escolha por categoria:</h2>
-      <Carousel activeIndex={index} onSelect={handleSelect}>
-        {listaProjetos.map((item, i)=>{
-        const{imagem, nome} = item;
-          return(
-            <Carousel.Item>
-                <img className= "d-block" 
-                    src= {imagem} width= "150" height= "150" 
-                    alt= "nome"/>
-                <Carousel.Caption>
-                    <span className="nome">{nome}</span>
-                </Carousel.Caption>
-            </Carousel.Item>
-          )
-        })}
-                
+  return (
+    <section id="carrossel">
+      <h2>Produtos em destaque:</h2>
+      <Carousel>
+        <Carousel.Item>      
+          <Row>
+            <Col md={3} sm={6}>
+              <img
+              className="d-block"
+              src= {cacto02} width= "150" height= "150"
+              alt="cacto"
+              />
+            </Col>
+            <Col md={3} sm={6} className="d-none d-sm-block">
+              <img
+              className="d-block"
+              src= {suculenta2} width= "150" height= "150"
+              alt="suculenta"
+              />
+            </Col>
+            <Col md={3} sm={6} className="d-none d-md-block">
+              <img
+              className="d-block"
+              src= {samambaia} width= "150" height= "150"
+              alt="samambaia"
+              />
+            </Col>
+          </Row>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Row>
+            <Col md={3} sm={6}>
+              <img
+              className="d-block"
+              src= {flor04} width= "150" height= "150"
+              alt="flor"
+              />
+            </Col>
+            <Col md={3} sm={6} className="d-none d-sm-block">
+              <img
+              className="d-block"
+              src= {designSemNome2} width= "150" height= "150"
+              alt="utensílios"
+              />
+            </Col>
+            <Col md={3} sm={6} className="d-none d-md-block">
+              <img
+              className="d-block"
+              src= {cacto02} width= "150" height= "150"
+              alt="cacto"
+              />
+            </Col>
+          </Row>
+        </Carousel.Item>
       </Carousel>
-      </section>
+    </section>
         
-    );
+  )
 }
     
 export default Carrossel;
