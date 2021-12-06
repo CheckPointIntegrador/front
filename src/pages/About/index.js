604594c456917fbe8dfa1a41b9c950fdf018a009
lipollis <aline.pollis@hotmail.com>
2021-12-05 23:54:31 -0300

    acrescentando o import em pages/about

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,12 +1,13 @@
 import { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import BannerHome from '../../components/AboutUs/BannerAbout'
 import AboutUs from '../../components/AboutUs';
 
 
 const About = () => {
   return (
     <>
-      <Link to="/">Home</Link>
+      {/* <Link to="/">Home</Link> */}
       <BannerHome />
       <AboutUs />
     </>
