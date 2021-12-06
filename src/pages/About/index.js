b14f3fd5ee352ee5ee0b9adff30cabf36b442243
thaysgama <thays.s.gama@gmail.com>
2021-12-06 10:26:16 -0300

    apaga header e footer sobre nos

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,18 +1,15 @@
-import { Component } from 'react';
-// import { Link } from 'react-router-dom';
-import Header       from '../../components/Header';
 import BannerHome   from '../../components/AboutUs/BannerAbout';
 import AboutUs      from '../../components/AboutUs';
-import Footer       from '../../components/Footer';
+
 
 
 const About = () => {
   return (
-    <
-      <Header />
+    <>
+      
       <BannerHome />
       <AboutUs />
-      <Footer />
+      
     </>
   );
 }
