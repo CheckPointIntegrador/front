874949479bcaadaef8865c3655cdb250da254fe6
thaysgama <thays.s.gama@gmail.com>
2021-12-07 19:06:49 -0300

    adiciona pagina 404

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,15 +1,28 @@
 import { useNavigate } from 'react-router-dom';
+import './style.scss';
+import plant from '../../imgs/notFound/planta-esther.png'
+import { Container } from 'react-bootstrap';
+
 
 const NotFound = () => {
   const navigate = useNavigate();
-
+ 
   setTimeout(() => {
     navigate('/');
-  }, 5000);
+  }, 6000);
 
   return (
     <>
-      <h2>404 nada por aqui</h2>
+    <Container xs={12} sm={8} className="not-found d-flex flex-wrap justify-content-center">
+      <div className="m-auto px-4">
+        <h1 className="py-2">404</h1>
+        <h2 className="py-2">Página não encontrada.</h2>
+        <p className="py-2">A falecida Matilda pede que cuidem bem das suas plantinhas.</p>
+      </div>
+      <div data-aos="zoom-in-left" data-aos-duration="2000">
+        <img className="planta-carnivora py-2" src={plant} alt="planta-carnivora" />
+      </div>
+    </Container>
     </>
   )
 }
