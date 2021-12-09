f1a787d92cca6faf52bf80173aff491bbef2d8da
lipollis <aline.pollis@hotmail.com>
2021-12-08 22:11:13 -0300

    links do linkedin e hover

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -31,14 +31,14 @@ const AboutUs = () => {
                         <div className="caixa caixa-1">
                             <div className="image-container-about col-lg-6 col-xm-12">
                                 <div className="photo-wrap mb-5">
-                                    <img className="profile-img" src={Aline} alt="Minha foto." />
+                                    <img className="profile-img" src={Aline} alt="Foto Aline Pollis." />
                                 </div>
                             </div>
                             <div className=" retangle">
                                 <div className="col-lg-6 col-xm-12 aboutme ">
                                     <h4 className="profile-name">Aline Pollis</h4>
-                                    <h5 className="profile-social"><a href="">LinkedIn</a></h5>
-                                    <h5 className="profile-social"><a href="http://github.com/lipollis">GitHub</a></h5>
+                                    <h5 className="profile-social"><a href="https://www.linkedin.com/in/aline-pollis-beck/" target="_blank">LinkedIn</a></h5>
+                                    <h5 className="profile-social"><a href="http://github.com/lipollis" target="_blank">GitHub</a></h5>
                                 </div>
                             </div>
                         </div>
@@ -48,14 +48,14 @@ const AboutUs = () => {
                         <div className="caixa caixa-2">
                             <div className="image-container-about col-lg-6 col-xm-12">
                                 <div className="photo-wrap mb-5">
-                                    <img className="profile-img" src={Esther} alt="Minha foto." />
+                                    <img className="profile-img" src={Esther} alt="Foto Esther Pimentel." />
                                 </div>
                             </div>
                             <div className=" retangle">
                                 <div className="col-lg-6 col-xm-12 aboutme">
                                     <h4 className="profile-name">Esther Pimentel</h4>
-                                    <h5 className="profile-social"><a href="">LinkedIn</a></h5>
-                                    <h5 className="profile-social"><a href="https://github.com/EstherPimentel">GitHub</a></h5>
+                                    <h5 className="profile-social"><a href=" https://www.linkedin.com/in/esther-maria-pimentel-porto-4b351a74/" target="_blank">LinkedIn</a></h5>
+                                    <h5 className="profile-social"><a href="https://github.com/EstherPimentel" target="_blank">GitHub</a></h5>
                                 </div>
                             </div>
                         </div>
@@ -65,14 +65,14 @@ const AboutUs = () => {
                         <div className="caixa caixa-3">
                             <div className="image-container-about col-lg-6 col-xm-12">
                                 <div className="photo-wrap mb-5">
-                                    <img className="profile-img" src={Giselle} alt="Minha foto." />
+                                    <img className="profile-img" src={Giselle} alt="Foto Giselle Piasetzki." />
                                 </div>
                             </div>
                             <div className=" retangle">
                                 <div className="col-lg-6 col-xm-12 aboutme">
                                     <h4 className="profile-name">Giselle Piasetzki</h4>
-                                    <h5 className="profile-social"><a href="https://www.linkedin.com/in/gisellepiasetzki/">LinkedIn</a></h5>
-                                    <h5 className="profile-social"><a href="https://github.com/GisellePiasetzki">GitHub</a></h5>
+                                    <h5 className="profile-social"><a href="https://www.linkedin.com/in/gisellepiasetzki/" target="_blank">LinkedIn</a></h5>
+                                    <h5 className="profile-social"><a href="https://github.com/GisellePiasetzki" target="_blank">GitHub</a></h5>
                                 </div>
                             </div>
                         </div>
@@ -82,14 +82,14 @@ const AboutUs = () => {
                         <div className="caixa caixa-4">
                             <div className="image-container-about col-lg-6 col-xm-12">
                                 <div className="photo-wrap mb-5">
-                                    <img className="profile-img" src={Leticia} alt="Minha foto." />
+                                    <img className="profile-img" src={Leticia} alt="Foto Letícia Carmo." />
                                 </div>
                             </div>
                             <div className=" retangle">
                                 <div className="col-lg-6 col-xm-12 aboutme">
                                     <h4 className="profile-name">Letícia Carmo</h4>
-                                    <h5 className="profile-social"><a href="">LinkedIn</a></h5>
-                                    <h5 className="profile-social"><a href="https://github.com/liacarmo">GitHub</a></h5>
+                                    <h5 className="profile-social"><a href="https://www.linkedin.com/in/leticia-carmo-/" target="_blank">LinkedIn</a></h5>
+                                    <h5 className="profile-social"><a href="https://github.com/liacarmo" target="_blank">GitHub</a></h5>
                                 </div>
                             </div>
                         </div>
@@ -99,14 +99,14 @@ const AboutUs = () => {
                         <div className="caixa caixa-5">
                             <div className="image-container-about col-lg-6 col-xm-12">
                                 <div className="photo-wrap mb-5">
-                                    <img className="profile-img" src={Silas} alt="Minha foto." />
+                                    <img className="profile-img" src={Silas} alt="Foto Silas Medeiros." />
                                 </div>
                             </div>
                             <div className=" retangle">
                                 <div className="col-lg-6 col-xm-12 aboutme">
                                     <h4 className="profile-name">Silas Medeiros</h4>
-                                    <h5 className="profile-social"><a href="https://www.linkedin.com/in/silas-medeiros-6b44a0213/">LinkedIn</a></h5>
-                                    <h5 className="profile-social"><a href="https://github.com/silasms">GitHub</a></h5>
+                                    <h5 className="profile-social"><a href="https://www.linkedin.com/in/silas-medeiros-6b44a0213/" target="_blank">LinkedIn</a></h5>
+                                    <h5 className="profile-social"><a href="https://github.com/silasms" target="_blank">GitHub</a></h5>
                                 </div>
                             </div>
                         </div>
@@ -116,14 +116,14 @@ const AboutUs = () => {
                         <div className="caixa caixa-6">
                             <div className="image-container-about col-lg-6 col-xm-12">
                                 <div className="photo-wrap mb-5">
-                                    <img className="profile-img" src={Thays} alt="Minha foto." />
+                                    <img className="profile-img" src={Thays} alt="Foto Thays Gama." />
                                 </div>
                             </div>
                             <div className=" retangle">
                                 <div className="col-lg-6 col-xm-12 aboutme">
                                     <h4 className="profile-name">Thays Gama</h4>
-                                    <h5 className="profile-social"><a href="">LinkedIn</a></h5>
-                                    <h5 className="profile-social"><a href="https://github.com/thaysgama">GitHub</a></h5>
+                                    <h5 className="profile-social"><a href="https://www.linkedin.com/in/thaysgama/" target="_blank">LinkedIn</a></h5>
+                                    <h5 className="profile-social"><a href="https://github.com/thaysgama" target="_blank">GitHub</a></h5>
                                 </div>
                             </div>
                         </div>
