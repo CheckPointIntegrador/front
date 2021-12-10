921d9fc62a121d2c284e24e382c01f73541bc42d
silasms <silas.medeiros7@gmail.com>
2021-12-10 17:47:09 -0300

    Mudando para o heroku

--- src/services/api.js
+++ src/services/api.js
@@ -12,7 +12,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'http://casaplantas-env.eba-mysshnpd.us-east-1.elasticbeanstalk.com'
+  baseURL: 'https://casadasplantinhas.herokuapp.com/'
 });
 
 export default api;
